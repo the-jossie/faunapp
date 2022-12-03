1ca3a1caded4d3447b27fedb38cd18e0e4da2e9e
Dev Jo <idowuayanfeoluwa@gmail.com>
2022-12-03 18:28:57 +0100

    feat: splash screen

--- lib/views/splash_view.dart
+++ lib/views/splash_view.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
-import '../config/app_config.dart';
 import '../config/size_config.dart';
-import '../formatters/num.dart';
 import '../view_models/splash_view_model.dart';
 
 class SplashView extends StatelessWidget {
@@ -16,25 +14,15 @@ class SplashView extends StatelessWidget {
       viewModelBuilder: () => SplashViewModel(),
       onModelReady: (model) => model.init(),
       builder: (context, viewModel, child) {
-        return Scaffold(
-          body: Container(
-            height: 100.h,
-            width: 100.w,
-            alignment: Alignment.center,
-            color: AppConfigService.hexToColor("#ffb300"),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Spacer(),
-                Text("Faunapp"),
-                Spacer(),
-                Text("Everything animals!"),
-                SizedBox(
-                  height: 30,
-                ),
-              ],
+        return Stack(
+          children: <Widget>[
+            Image.asset(
+              "assets/images/splash_bg.png",
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,
             ),
-          ),
+          ],
         );
       },
     );
